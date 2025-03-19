8009ab89d99ecfcd95d798b0bee66b55570ff3e6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-19 18:56:44 +0900

    Feat: github 이미지 패턴 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+    
+  },
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:"avatars.githubusercontent.com",
+        pathname:"**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
