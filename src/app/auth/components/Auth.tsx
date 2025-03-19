c99ceb84fa8f37b894629beb770ae2546ecc1028
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-19 19:00:00 +0900

    Feat: Auth 기본 컴포넌트

---
+++ src/app/auth/components/Auth.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import SignInButton from './Sign-in-button'
+
+const Auth = () => {
+  return (
+    <div>
+      Client Component
+      <div>
+
+        <p>Not Signed</p>
+        <SignInButton/>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
