ba5c21f27e7e7d98a0b99ec0143ffa15fed60a29
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-19 19:00:26 +0900

    Feat: Auth 추가

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,16 @@
 import Link from 'next/link'
 
 const links = [
-  { href: '/home', label: 'Home' },
+  { href: '/', label: 'Home' },
   { href: '/page1', label: 'Page1' },
   { href: '/draft', label: 'Draft' },
+  { href: '/auth', label: 'Auth' },
 ]
 
 export default function Header() {
   return (
     <header>
-      <nav className="flex gap-x-2 p-2">
+      <nav className="flex gap-x-2 p-2 border-b-2">
         {links.map(link => (
           <Link
             key={link.label}
